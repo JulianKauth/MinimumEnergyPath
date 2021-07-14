2bf526e24ddb2166111e3c730c6a8c51d4f4cdc7
Julian Kauth <julian.kauth@fau.de>
2021-07-15 00:02:51 +0200

    add some logging to the main method

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use crate::pes::Gaussian;
 use crate::image::Image;
 use crate::chain::Chain;
 use crate::point::Point;
+use std::time::SystemTime;
 
 mod point;
 mod pes;
@@ -10,6 +11,9 @@ mod image;
 mod chain;
 
 fn main() {
+    // Important config at the top: convergence limit, PES, Chain, Image Config
+    let stable_limit = 1e-6;
+
     //create mep
     let pes = PES {
         gaussians: vec![
@@ -24,14 +28,21 @@ fn main() {
         Point { x: 0.0, y: 7.5 },
         20,
     );
+
+    // keep track of how long everything takes
+    let mut start_time = SystemTime::now();
+
     //set up our image generator
+    print!("Image setup took: ");
     let img = Image::new(-2.0, 9.0, -2.0, 9.0, 2000, 2000, &pes);
+    print_elapsed_time(&mut start_time);
+    println!();
 
     // iterate until we reached a stable state
-    let stable_limit = 1e-6;
     let mut counter = 0;
     let mut last_energy;
     let mut energy = chain.energy(&pes);
+    println!("starting with initial energy: {}", energy);
     loop {
         // save the state
         img.paint(&*format!("../images/progress_{:04}.png", counter), &chain, &pes);
@@ -43,11 +54,23 @@ fn main() {
         counter += 1;
         last_energy = energy;
         energy = chain.energy(&pes);
-        println!("{}", energy);
+
+        // print info
+        print!("iteration: {:.6} resulted in energy: {} and took: ", energy, energy);
+        print_elapsed_time(&mut start_time);
+        println!();
 
         // stop the loop if the last iteration was barely able to improve the situation
-        if last_energy - energy < stable_limit{
+        if last_energy - energy < stable_limit {
             break;
         }
     }
+}
+
+fn print_elapsed_time(time_instance: &mut SystemTime) {
+    match time_instance.elapsed() {
+        Ok(elapsed) => { print!("{}.{:03} sec", elapsed.as_secs(), elapsed.subsec_millis()); }
+        Err(_) => { print!("<error>"); }
+    }
+    *time_instance = SystemTime::now();
 }
