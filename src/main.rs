bc1aed37c9dd6b101393c49ed7fedc9aca9ac66d
Julian Kauth <julian.kauth@fau.de>
2021-07-15 21:00:10 +0200

    add help text and json explanation

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,13 @@ use crate::image::{Image, ImageConfig};
 use crate::chain::{Chain, ChainConfig};
 use crate::point::Point;
 use std::time::SystemTime;
-use std::fs;
+use std::{fs, env};
 use std::process::exit;
 
 mod point;
 mod pes;
 mod image;
 mod chain;
-
-//todo: make help text
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Config {
@@ -26,6 +24,9 @@ struct Config {
 }
 
 fn main() {
+    // argument parsing first, in case we need to catch a --help and exit early
+    arg_parse();
+
     // keep track of how long everything takes
     let mut start_time = SystemTime::now();
 
@@ -87,7 +88,7 @@ fn ensure_image_directory() {
     if let Err(err) = std::fs::create_dir_all(std::path::Path::new("images/")) {
         println!("Could not create the directory for the resulting images!");
         println!("Error: {}", err);
-        exit(4);
+        exit(3);
     }
 }
 
@@ -118,14 +119,15 @@ fn load_config() -> Config {
             match create_sample_config_file() {
                 Ok(_) => {
                     println!("Created an example config file 'MEP_config.txt'!");
-                    exit(2);
+                    println!("Simulating the example...");
+                    sample_config()
                 }
                 Err(err) => {
                     println!("Failed to create an example config file called 'MEP_config.txt'!");
                     println!("Error: {}", err);
                     println!("Please create the file manually. It has to conform to the following structure:");
                     println!("{}", sample_config_text());
-                    exit(3);
+                    exit(2);
                 }
             }
         }
@@ -141,10 +143,10 @@ fn sample_config_text() -> String {
 }
 
 fn sample_config() -> Config {
-    let stable_limit = 1e-3;
+    let stable_limit = 1e-4;
 
     let pes = PES {
-        scale: 0.3,
+        scale: 0.7,
         gaussians: vec![
             Gaussian { a: -7.0, x0: 15.0, sig_x: 5.0, y0: 7.5, sig_y: 5.0 },
             Gaussian { a: -13.0, x0: 23.0, sig_x: 2.0, y0: 5.0, sig_y: 2.0 },
@@ -183,4 +185,111 @@ fn sample_config() -> Config {
         path: chain_config,
         image: image_config,
     }
+}
+
+fn arg_parse() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {  // <=1, because the first one is always the program name
+        return;
+    }
+    // we got an argument
+    match &args[1][..] {
+        "--explain-json" => {
+            print_json_help();
+            exit(0);
+        }
+        _ => {
+            print_help_text();
+            exit(0);
+        }
+    }
+}
+
+fn print_help_text() {
+    let help_text = r#"
+    Simulation of finding the Minimum Energy Path on a Potential Energy Surface
+
+    usage:
+    minimum_energy_path                   simulate according to the config file
+    minimum_energy_path ( --help | -h )   show this help
+    minimum_energy_path --explain-json    explain the config file
+    "#;
+    println!("{}", help_text);
+}
+
+fn print_json_help() {
+    let json_explain = r#"
+    The config file is used to configure the simulation as well as the process of saving the results.
+
+    If no config file is found an example one is created which shows approximately the energy
+    landscape that was used in the lecture.
+
+    Most numbers can be given in floating point notation with the exception of the number of elements
+    in the path and the resolution of the image. Units are all refering to the simulation. The
+    resolution is only a mapping onto a canvas.
+
+    The structure of the file has to be matched precisely, because I didn't implement any smart parsing.
+
+    Now onto explaining the options available in the config file. The example config will look similar
+    to this:
+
+    {
+      "convergence_limit": 1e-3,        // if the change in average energy after a simulation step is
+                                        // smaller than this value the simulation will end.
+      "pes": {
+        "scale": 0.1,                   // by how much the PES should be scaled. As we are doing a
+                                        // simple gradient descent this is the way to set the speed
+                                        // and inversely to that the stability of the simulation
+
+        "gaussians": [                  // The PES is a linear combination of gaussian functions.
+          {"a": -7.0,                   // Each function has a preexponential factor a, the peak
+            "x0": 15.0,"sig_x": 5.0,    // (or valley) position (x0|y0) as well as the standard
+            "y0": 7.5,"sig_y": 5.0      // deviation in each direction.
+          },
+          //....
+          {"a": 10.0,
+            "x0": 0.0,"sig_x": 3.0,
+            "y0": 3.0,"sig_y": 3.0
+          }
+        ]
+      },
+      "path": {                         // The path describes the inital guess for the MEP.
+        "spring_constant": 0.3,         // How much the spring forces should be rescaled. Setting this
+                                        // to zero disables springs. Large values lead to instability.
+        "pin_ends": true,               // If the end are allowed to move. The ends do not have a
+                                        // proper tangent.
+        "relax_ends": true,             // If the ends should relax to their local minimum before they
+                                        // are used to construct the line of points. This can greatly
+                                        // improve results. Convergence criterium for having found the
+                                        // local minimum is the same as is set for the whole simulation
+        "start": {                      // Start ...
+          "x": 21.5,
+          "y": 5.5
+        },
+        "end": {                        // and end points of the line.
+          "x": 9.5,
+          "y": 19.5
+        },
+        "elements": 50                  // How many points are placed along the line. The program can
+                                        // deal with many, but large numbers (more than 100) lead to
+                                        // severe instability.
+      },
+      "image": {                        // This section configures the images that are saved
+        "contour_lines": 10,            // how many "contour lines" should be shown. They are not real
+                                        // contour lines, but instead the value is simply wrapped
+                                        // around, turning from white to black. Setting this to 1 will
+                                        // result in a grayscale image.
+        "x0": 0.0,                      // Where the bottom left corner of the image should be,
+        "y0": 0.0,
+        "width": 27.0,                  // how large of an area should be depicted...
+        "height": 25.0,
+        "resolution_x": 2700,           // and with which resolution this area should be rendered.
+        "resolution_y": 2500,           // The resulution is usually the bottleneck for the simulation.
+        "point_size": 0.1,              // How large the points along the line should be draw,
+        "line_width": 0.02              // and how large the lines. Important: This is in the same
+                                        // units as the PES, not in pixel units.
+      }
+    }
+    "#;
+    println!("{}", json_explain);
 }
